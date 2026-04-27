c6f794b228088a11ded011cb66172de815d9199a
Wajahat Naqvi <wajahatalinaqvi929@gmail.com>
2026-04-27 21:36:16 +0500

    Add missing line break in application routing configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,6 +9,7 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        
     )
   // Source - https://stackoverflow.com/a/78686097
 // Posted by reza_qsr
