ce3c1716733adda7b9032494c6e73ea0bca914aa
Wajahat Naqvi <wajahatalinaqvi929@gmail.com>
2026-04-28 16:23:23 +0500

    refactor: reorganize API routes and remove unnecessary middleware

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::withoutMiddleware(['web'])->group(function () {
-    Route::post('/exercise-1-artwork-version', [ArtWorkController::class, 'index'])->name('artwork.submit' );
-    Route::post('/exercise-2-tier-pricing', [ArtWorkController::class, 'pricing'])->name('pricing.submit' );
-});
+
